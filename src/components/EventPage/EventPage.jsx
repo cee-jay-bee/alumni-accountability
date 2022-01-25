800d1818831698d2150956692891c14068d8bc34
CJ Barnes <cristopher.barnes@gmail.com>
2022-01-25 11:53:13 -0600

    card styling created

--- src/components/EventPage/EventPage.jsx
+++ src/components/EventPage/EventPage.jsx
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
-import {useSelector} from 'react-redux';
+import React, { useState, useEffect } from 'react';
+import {useSelector, useDispatch} from 'react-redux';
 import { Link } from 'react-router-dom';
 //MATERIAL UI IMPORTS
 import { TextField, FormControl, MenuItem, Button, InputLabel, Select, Grid, Card, CardContent, CardActions, Typography, Modal, Box } from '@mui/material';
 import CreateNewEvent from '../CreateNewEvent/CreateNewEvent';
+import './EventPage.css';
 
 // Basic functional component structure for React with default state
 // value setup. When making a new component be sure to replace the
@@ -11,7 +12,21 @@ import CreateNewEvent from '../CreateNewEvent/CreateNewEvent';
 function EventPage(props) {
   // Using hooks we're creating local state for a "heading" variable with
   // a default value of 'Functional Component'
-  const store = useSelector((store) => store);
+  const dispatch = useDispatch();
+  const event = useSelector((store) => store.event);
+
+  useEffect(() => {
+    dispatch({ type: 'FETCH_EVENT'});
+  }, []);
+
+  let today = new Date();
+  console.log(today);
+  // let dd = String(today.getDate()).padStart(2, '0');
+  // let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+  // let yyyy = today.getFullYear();
+
+  // today = yyyy + '-' + mm + '-' + dd;
+  
 
   //HANLDE POP-UP MODAL
   const [open, setOpen] = React.useState(false);
@@ -28,8 +43,112 @@ function EventPage(props) {
         <p>Want to view all events from the beginning of time? Click here</p>
       </Link>
 
-      <h3>Here is a list of FSE events</h3>
-      <h3>Here is a list of UX events</h3>
+      <h3>Events Needing Attendance</h3>
+      <section className="event">
+
+        {event.map(event => {
+
+          let eventDate = new Date(event.date);
+          console.log(eventDate);
+        
+          if(event.confirm_attendance === false && eventDate <= today) {
+            const setOneEvent = () => {
+              dispatch({
+                type: 'SET_ONE_EVENT',
+                payload: {
+                  id: event.id,
+                  title: event.title,
+                  date: event.date,
+                  time: event.time, 
+                  stack_type: event.stack_type,
+                  description: event.description
+                }
+              })
+            }
+            return (
+              
+              <Grid
+              container
+              style={{'flex-flow': 'row', 'width': '18em'}}
+              direction="row"
+              justifyContent="flex-start"
+              display="inline-flex"
+              alignItems="center">
+                {/* <Grid item xs={3}> */}
+                  <Card style={{'height': '250px'}}>
+                    <CardContent>
+                      <Typography>
+                        Event Id: {event.id}
+                        Event Title: {event.title}
+                        Event Date: {event.date}
+                        Event Time: {event.time}
+                        Event Stack Type: {event.stack_type}
+                        Event Description: {event.description}
+                      </Typography>
+                    </CardContent>
+                  </Card>
+                {/* </Grid> */}
+              </Grid>
+              // <div>
+              //   <p>Event Id: {event.id}</p>
+              //   <p>Event Title: {event.title}</p>
+              //   <p>Event Date: {event.date}</p>
+              //   <p>Event Time: {event.time}</p>
+              //   <p>Event Stack Type: {event.stack_type}</p>
+              //   <p>Event Description: {event.description}</p>
+              // </div>
+            )
+          }
+        })
+      }
+      </section>
+
+      
+      <h3>Upcoming Events</h3>
+
+      <section className="event">
+        {event.map(event => {
+          let eventDate = new Date(event.date);
+          if(event.confirm_attendance === false && eventDate > today) {
+            const setOneEvent = () => {
+              dispatch({
+                type: 'SET_ONE_EVENT',
+                payload: {
+                  id: event.id,
+                  title: event.title,
+                  date: event.date,
+                  time: event.time, 
+                  stack_type: event.stack_type,
+                  description: event.description
+                }
+              })
+            }
+            return (
+              <Grid
+              container
+              style={{'flex-flow': 'row', 'width': '18em'}}
+              direction="row"
+              justifyContent="flex-start"
+              display="inline-flex"
+              alignItems="center">
+                {/* <Grid item xs={3}> */}
+                  <Card style={{'height': '250px'}}>
+                    <CardContent>
+                      <Typography>Event Id: {event.id}</Typography>
+                        <Typography>Event Title: {event.title}</Typography>
+                        <Typography>Event Date: {event.date}</Typography>
+                        <Typography>Event Time: {event.time}</Typography>
+                        <Typography>Event Stack Type: {event.stack_type} </Typography>
+                        <Typography>Event Description: {event.description}</Typography>
+                    </CardContent>
+                  </Card>
+                {/* </Grid> */}
+              </Grid>
+            )
+          }
+        })
+      }
+      </section>
 
       <h2 onClick={handleClickOpen}>Let's create a new event</h2>
 
---
+++ src/redux/reducers/oneEvent.reducer.js
@@ -0,0 +1,14 @@
+const eventReducer = (state = [], action) => {
+  switch (action.type) {
+    case 'SET_ONE_EVENT':
+      return action.payload;
+    case 'UNSET_ONE_EVENT':
+      return [];
+    default:
+      return state;
+  }
+};
+
+// user will be on the redux state at:
+// state.user
+export default eventReducer;
