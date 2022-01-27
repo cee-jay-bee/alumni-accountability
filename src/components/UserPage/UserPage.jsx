ef4117076bf9f995c9c71dcc833fd09a39adb7d2
Paran Kashani <parankashani@gmail.com>
2022-01-27 13:15:15 -0600

    home page init styling done-reminder sidebar setup complete

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,27 +1,92 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import {useSelector} from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+//IMPORT CSS
+import './UserPage.css'
 
 function UserPage() {
   // this component doesn't do much to start, just renders some user reducer info to the DOM
   const user = useSelector((store) => store.user);
-  console.log( location.href );
+  const event = useSelector((store) => store.event);
+  const dispatch = useDispatch();
+  const history= useHistory();
+
+  useEffect(() => {
+    dispatch({ type: 'FETCH_EVENT'});
+    dispatch({ type: 'FETCH_ALUM'});
+  }, []);
+
+  let today = new Date();
 
   return (
-    <div id="homepage" className="container">
-      <h2>Welcome, {user.firstname} {user.lastname}!</h2>
-      <p>Your ID is: {user.id}</p>
       <div>
-        <Link to="/eventpage">
-          <h1>Events</h1>
-        </Link>
-        <Link to="cohortpage">
-          <h1>Cohorts</h1>
-        </Link>
+        <div className="homepageHeader">
+          <h2>Welcome, {user.firstname} {user.lastname}!</h2>
+          {/* <p>Your ID is: {user.id}</p> */}
+        </div>
+        <main className="mainDivHomePage" >
+          
+            <div className ="mainDivHomePageCol1" onClick={goToEvents}>
+              <h1>Events</h1>
+            </div>
+          <div className ="mainDivHomePageCol2">
+            <Link to="cohortpage">
+              <h1>Cohorts</h1>
+            </Link>
+          </div>
+          <div className="mainDivHomePageCol3">
+            <h3 classname="mainPageCol3Header">Events Needing Attendance</h3>
+            {event.map(event => {
+
+              let eventCompareDate = new Date(event.date);
+              let twoDigitMonth = eventCompareDate.getMonth() + 1 + "";
+              let twoDigitDate = eventCompareDate.getDate() + "";
+              if (twoDigitDate.length == 1){
+                twoDigitDate = "0" + twoDigitDate;
+              }
+              let eventDate = twoDigitMonth + "/" + twoDigitDate + "/" + eventCompareDate.getFullYear(); 
+              console.log(eventDate);
+
+              if(event.confirm_attendance === false && eventCompareDate <= today) {
+                const setOneEvent = () => {
+                  dispatch({
+                    type: 'SET_ONE_EVENT',
+                    payload: {
+                      id: event.id,
+                      title: event.title,
+                      date: event.date,
+                      time: event.time, 
+                      stack_type: event.stack_type,
+                      description: event.description
+                    }
+                  })
+                }
+                return (
+                
+                  <div className="mainPageEventItem">
+                    <p className="mainPageDateStyling"  >{eventDate} {event.time.toLocaleString('en-US')}
+                    {(event.stack_type === 'FSE') ?
+                      <p class="mainPageStackTypeDisplay">FSE</p> :
+                      (event.stack_type === 'UX/UI') ?
+                      <p class="mainPageStackTypeDisplay">UX/UI</p> :
+                      <span><p class="mainPageStackTypeDualDisplay">UX/UI</p> <p class="mainPageStackTypeDualDisplay">FSE</p></span>
+                    }
+                    </p>
+
+                    {(event.title.length > 15) ?
+                      <h2 class="mainPageTitleStyling">{event.title.slice(0,15)}...</h2> :
+                      <h2 class="mainPageTitleStyling">{event.title}</h2>
+                    }
+                  </div>
+                )
+              }
+            })}
+          </div>
+          {/* <LogOutButton className="btn" /> */}
+        </main>
       </div>
-      <LogOutButton className="btn" />
-    </div>
   );
 }
 
