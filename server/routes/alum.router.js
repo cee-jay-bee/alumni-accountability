9c6f524e51dbb1a303732c577048a6269bceb98a
jcahill612 <jenny.e.cahill@gmail.com>
2022-01-26 19:12:54 -0600

    alum router

--- server/routes/alum.router.js
+++ server/routes/alum.router.js
@@ -1,19 +1,39 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
+const {
+  rejectUnauthenticated,
+} = require('../modules/authentication-middleware');
 
 /**
  * GET route template
  */
-router.get('/', (req, res) => {
-  // GET route code here
+router.get('/', rejectUnauthenticated, (req, res) => {
+
+  const query = `SELECT * FROM alum JOIN cohort on alum.cohort_id = cohort.id`;
+  pool.query(query)
+    .then( result => {
+      res.send(result.rows);
+    })
+    .catch(err => {
+      console.log('ERROR: Get alum', err);
+      res.sendStatus(500)
+    })
 });
 
-/**
- * POST route template
- */
-router.post('/', (req, res) => {
-  // POST route code here
-});
+
+router.post('/', rejectUnauthenticated, (req, res) => {
+
+    const { name, placed, seeking, cohortId } = req.body
+    const queryText = `INSERT INTO "alum" (name, placed, seeking, cohort_id) VALUES ($1 , $2, $3, $4)`
+    pool.query(queryText,[ name, placed, seeking, cohortId]).then(()=>
+        res.sendStatus(201)
+    ).catch(err=>{
+        console.log("alum post router has error", err)
+        res.sendStatus(500)
+        }
+    )
+  });
+
 
 module.exports = router;
