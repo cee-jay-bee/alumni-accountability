d6995e19328858ea5bd990594787b03a4c25558e
Rachel Hoey <rhoey@hotmail.com>
2022-01-28 13:30:01 -0600

    tag textfield placed

--- src/components/EventTags/EventTags.jsx
+++ src/components/EventTags/EventTags.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
-import {useSelector} from 'react-redux';
+import React, { useState, useEffect } from 'react';
+import {useSelector, useDispatch} from 'react-redux';
+import { TextField} from '@mui/material';
 
 // Basic functional component structure for React with default state
 // value setup. When making a new component be sure to replace the
@@ -11,11 +12,66 @@ function EventTags(props) {
   const oneEvent = useSelector((store) => store.oneEvent);
   
 
-  return (
-    <div>
-      <h2>Tags</h2>
+  const dispatch = useDispatch();
 
-    </div>
+  //EVENT TAG HOOK
+  const [eventTag, setEventTag] = useState('');
+
+  useEffect(() => {
+    dispatch({ type: 'FETCH_TAG'});
+  }, []);
+
+//   function handleSubmit(event){
+//     event.preventDefault();
+//     console.log(newInventory)
+    
+// }
+// const handleKeyDown = (ev)=>{
+//    if(ev.keyCode ===''){ // enter button
+//     formRef.current.submit()
+//    }
+// }
+  
+
+  const addEventTag = (event) => {
+        dispatch({
+        type: 'POST_TAG',
+        payload: {
+          id: oneEvent.id,
+          event_tag_tag: eventTag
+          
+          },
+    });
+  }
+  
+
+  return (
+    <main>
+      <div className='tagsHeader'>
+      <h2>Tag</h2>
+      <div className='taginputfield'>
+        {/* EVENT TAG INPUT */}
+        
+        <TextField
+              // id="outlined-multiline-static"
+              className="createNewEventTag"
+              style={{'left':'500px', width: '500px', top: '-45px'}}
+              size='small'
+              label="add event tag"
+              variant="outlined"
+              autoComplete= "off"
+              // // variant="filled"
+              // // color="warning"
+              // style ={{width: '150%', justifyItems: 'center', marginLeft: '-25%'}}
+              type="text"
+              name="event tag"
+              required
+              value={eventTag}
+              onChange={(event) => setEventTag(event.target.value)}
+            />
+      </div>
+      </div>
+    </main>
   );
 }
 
