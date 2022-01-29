a416e278f403210991dbcbf27367fa02178b78ac
jcahill612 <jenny.e.cahill@gmail.com>
2022-01-29 11:38:48 -0600

    alum saga

--- src/redux/sagas/alum.saga.js
+++ src/redux/sagas/alum.saga.js
@@ -3,11 +3,28 @@ import { put, takeLatest } from 'redux-saga/effects';
 
 // worker Saga: will be fired on "FETCH_USER" actions
 function* fetchAlum() {
-  
+  try {
+    const response = yield axios.get(`/api/alum`);
+    yield put({ type: 'SET_ALUM', payload: response.data });
+  } catch (error) {
+    console.log('Alum get request failed', error);
+  }
 }
+
+
+function* createAlum(action) {
+  try {
+    yield axios.post('/api/alum', action.payload);
+    yield put({ type: 'FETCH_ALUM' })
+  } catch (error) {
+    console.log('Alum Post request failed', error);
+  }
+}
+
 
 function* alumSaga() {
   yield takeLatest('FETCH_ALUM', fetchAlum);
+  yield takeLatest('CREATE_ALUM', createAlum);
 }
 
 export default alumSaga;
