ca02979fab24e637a06e0c21c603a28407199be5
Francisco Lopez <frankie.b.lopez@gmail.com>
2022-01-29 11:18:36 -0600

    eventnote saga

--- src/redux/sagas/eventNote.saga.js
+++ src/redux/sagas/eventNote.saga.js
@@ -1,8 +1,14 @@
 import axios from 'axios';
 import { put, takeLatest } from 'redux-saga/effects';
 
-// worker Saga: will be fired on "FETCH_USER" actions
-function* fetchEventNote() {
+
+function* fetchEventNote(action) {
+  try {
+    const response = yield axios.get(`/api/eventNote/${action.payload}`);  
+    yield put({ type: 'SET_EVENTNOTE' ,payload : response.data})
+  } catch (error) {
+    console.log('Fetch event note request failed', error);
+  }
   
 }
 
