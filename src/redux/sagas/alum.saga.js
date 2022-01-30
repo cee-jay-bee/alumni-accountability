77f000907758e816a3d1f6d2630f2ffd0ce15f5f
jcahill612 <jenny.e.cahill@gmail.com>
2022-01-30 16:40:30 -0600

    revised alum route and saga

--- src/redux/sagas/alum.saga.js
+++ src/redux/sagas/alum.saga.js
@@ -7,24 +7,42 @@ function* fetchAlum() {
     const response = yield axios.get(`/api/alum`);
     yield put({ type: 'SET_ALUM', payload: response.data });
   } catch (error) {
-    console.log('Alum get request failed', error);
+    console.log('ALum get request failed', error);
   }
 }
-
 
 function* createAlum(action) {
   try {
     yield axios.post('/api/alum', action.payload);
-    yield put({ type: 'FETCH_ALUM' })
+    yield put({ type: 'FETCH_EVENT' })
   } catch (error) {
     console.log('Alum Post request failed', error);
   }
 }
 
+function* updateAlum(action) {
+  try {
+    yield axios.put(`/api/alum/${action.payload.id}`, action.payload);
+    yield put({ type: 'FETCH_EVENT' })
+  } catch (error) {
+    console.log('Alum put request failed', error);
+  }
+}
+
+function* deleteAlum(action) {
+  try {
+    yield axios.delete(`/api/alum/${action.payload}`);
+    yield put({ type: 'FETCH_EVENT' })
+  } catch (error) {
+    console.log('Alum Delete request failed', error);
+  }
+}
 
 function* alumSaga() {
   yield takeLatest('FETCH_ALUM', fetchAlum);
   yield takeLatest('CREATE_ALUM', createAlum);
+  yield takeLatest('DELETE_ALUM', deleteAlum);
+  yield takeLatest('UPDATE_ALUM', updateAlum);
 }
 
 export default alumSaga;
