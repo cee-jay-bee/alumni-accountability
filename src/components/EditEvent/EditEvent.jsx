472388ea25148022889e91b8522d2fd514cf9ce1
Paran Kashani <parankashani@gmail.com>
2022-02-04 17:52:34 -0600

    edit modal styled and ready for import

--- src/components/EditEvent/EditEvent.jsx
+++ src/components/EditEvent/EditEvent.jsx
@@ -1,18 +1,109 @@
 import React, { useState } from 'react';
 import {useSelector} from 'react-redux';
 import { Link } from 'react-router-dom';
+import { TextField, FormControl, MenuItem, Button, InputLabel, Select, Grid, Card, CardContent, CardActions, Typography, Modal, Box, Paper} from '@mui/material';
+import { useDispatch } from 'react-redux';
+//IMPORT CREATE NEW EVENT SCSS
+import './EditEvent.scss'
+
 
 // Basic functional component structure for React with default state
 // value setup. When making a new component be sure to replace the
 // component name EventPage with the name for the new component.
-function EditEvent(props) {
+function EditEvent() {
   // Using hooks we're creating local state for a "heading" variable with
   // a default value of 'Functional Component'
   const store = useSelector((store) => store);
+  // const event = useSelector((store) => event);
 
+    //HANLDE POP-UP MODAL
+    const [open, setOpen] = React.useState(false);
+    const handleClickOpen = () => {
+        setOpen(!open);
+    };
+    // END HANDLE POP-UP MODAL
+
+  const dispatch = useDispatch();
+  // const history = useHistory();
+  const addNewEvent = (event) => {
+    //     dispatch({
+    //     type: 'CREATE_EVENT',
+    //     payload: {
+    //       eventTitle: eventTitle,
+    //       eventDate: eventDate,
+    //       eventTime: eventTime,
+    //       eventDescription: eventDescription,
+    //       eventStackType: eventStackType,
+    //       eventTag: eventTag
+    //       },
+    // });
+      };
+
+//EVENT TITLE HOOK
+const [eventTitle, setEventTitle] = useState('');
+
+//EVENT DATE HOOK
+const [eventDate, setEventDate] = useState('');
+ 
+//EVENT STACK TYPE HOOK
+ const [eventStackType, setEventStackType] = useState('');
+
+//EVENT DESCRIPTION HOOK
+const [eventDescription, setEventDescription] = useState('');
+
+//EVENT TIME HOOK
+const [eventTime, setEventTime] = useState('');
+
+//EVENT TAG HOOK
+const [eventTag, setEventTag] = useState('');
+  
   return (
-    <div>
-      <h2>Let's edit this event</h2>
+    <div className="EditEventCard">
+      {/* EVENT NAME INPUT */}
+          <div className="createNewEventCardHeader">
+            <Link to="/eventdetail">
+              <div className="createNewEventclosing">
+                  <p>x</p>
+              </div>
+            </Link>
+            <div className="createNewEventH2div">
+              <h2 id="createNewEvenrH">Edit this event!</h2>
+            </div>
+          </div>
+          <div className="createNewEventTitleDateTimeDiv">
+            <input type="text" className="createnewtitleinput" placeholder="event title" label="event title"  autoComplete= "off" required value={eventTitle} onChange={(event) => setEventTitle(event.target.value)}/>
+            <input type="date" className="createnewDateInput" autoComplete= "off" required value={eventDate} onChange={(event) => setEventDate(event.target.value)} />
+            <input type="time" className="createnewTimeInput" autoComplete= "off"required value={eventTime} onChange={(event) => setEventTime(event.target.value)}/>
+          </div>
+          {/* EVENT DESCRIPTION INPUT */}
+          <div className="createNewDescriptionDiv">
+            <textarea type="text" className="createNewEventDescription" placeholder="event description" autoComplete= "off" maxRows={2} rows={7} wrap="soft" maxLength="250" required value={eventDescription} onChange={(event) => setEventDescription(event.target.value)}/>
+          </div>
+          {/* EVENT TAG INPUT */}
+          <div className="createNewEventTagDiv">
+            <input className="createNewEventTagInput" placeholder="event tags" type="text" autoComplete= "off" required value={eventTag} onChange={(event) => setEventTag(event.target.value)}/>
+          </div>
+          <div className="createnewFormandBtn">
+              {/* EVENT COHORT TYPE INPUT */}
+              <div>
+                {/* <label for="createnewdropdown">cohort type</label> */}
+                <select className="createnewdropdown" onChange={( event )=>setEventStackType( event.target.value )}>
+                  <option value="">cohort type</option>
+                  <option value={'FSE'}>FSE</option>
+                  <option value={'UX/UI'}>UX/UI</option>
+                  <option value={'FSE and UX/UI'}>FSE and UX/UI</option>
+                </select>
+              </div>
+              {/* SUBMISSION BTN */}
+              <div className="createNewEventBtn">
+                {/* {( eventTitle==='' || eventDate==='' || eventTime==='' || eventDescription==='' || eventStackType==='' || eventTag==='')? */}
+                {/* // <button className="createneweventdisable" variant="outlined" disabled>Create Event</button>: */}
+                <button className="createneweventsubmitbtn" variant="outlined" type="submit" name="submit" value="create event" onClick={addNewEvent}>Submit changes</button>
+                {/* } */}
+              </div>
+          </div>
+      <div class="createNewEventModalDiv">
+      </div>
     </div>
   );
 }
