c22fbd00a679a9303c059226706dd7a6b10ae28e
CJ Barnes <86207741+cee-jay-bee@users.noreply.github.com>
2022-02-07 19:10:34 -0600

    changes

--- src/components/RegisterPage/RegisterPage.jsx
+++ src/components/RegisterPage/RegisterPage.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
-
+import React, {useEffect} from 'react';
+import {useSelector} from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import RegisterForm from '../RegisterForm/RegisterForm';
 
 function RegisterPage() {
   const history = useHistory();
+  const user = useSelector((store) => store.user);
+
+  useEffect(() => {
+    dispatch({ type: 'FETCH_USER'});
+  }, []);
 
   return (
     <div>
@@ -12,8 +17,22 @@ function RegisterPage() {
       <div>
         <h2>Current users</h2>
       </div>
+      <div>
+        <table id='userTable'>
+          <tr class='userTable'>
+            <th id='userTableCol1'></th>
+            <th id='userTableCol2'>Name</th>
+            <th id='userTableCol3'>Status</th>
+            <th id='userTableCol4'>Delete</th>
+          </tr>
+        {user.map(user => 
+            (<UserItem key={user.id} alum={user} handleCheckboxChange={handleCheckboxChange}/>) 
+        )}
+      </table>
+    </div>
+    <div>
       <RegisterForm />
-
+    
       <center>
         <button
           type="button"
@@ -25,6 +44,7 @@ function RegisterPage() {
           Login
         </button>
       </center>
+      </div>
     </div>
   );
 }
