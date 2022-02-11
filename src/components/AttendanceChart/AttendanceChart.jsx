ecf7de20a121010c74174541708f9704f46dcd30
CJ Barnes <86207741+cee-jay-bee@users.noreply.github.com>
2022-02-10 20:12:22 -0600

    charts render each selection and update data and title

--- src/components/AttendanceChart/AttendanceChart.jsx
+++ src/components/AttendanceChart/AttendanceChart.jsx
@@ -3,23 +3,19 @@ import {useSelector, useDispatch} from 'react-redux';
 import { Link } from 'react-router-dom';
 import {Bar, Chart, Pie} from 'react-chartjs-2';
 import {Chart as ChartJS} from 'chart.js/auto';
-import AttendanceChart from '../AttendanceChart/AttendanceChart';
-
 // Basic functional component structure for React with default state
 // value setup. When making a new component be sure to replace the
-// component name DataPage with the name for the new component.
-function DataPage(props) {
+// component name TemplateFunction with the name for the new component.
+function AttendanceChart(props) {
   // Using hooks we're creating local state for a "heading" variable with
   // a default value of 'Functional Component'
+  const store = useSelector((store) => store);
+  const [heading, setHeading] = useState('Functional Component');
+
   const dispatch = useDispatch();
   const eventAttendance = useSelector((store) => store.eventAttendance);
   const event = useSelector((store) => store.event);
-  const [eventID, setEventID] = useState(0);
-  const [eventTitle, setEventTitle] = useState('');
-  
-  useEffect(() => {
-    dispatch({ type: 'FETCH_EVENT'});
-  }, []);
+
 
   let labels = [];
   for ( let i = 0; i < eventAttendance.length; i++) {
@@ -45,31 +41,30 @@ function DataPage(props) {
     ]
   }
 
-  const displayChart = () => {
-    dispatch({ type: 'FETCH_EVENT_ATTENDANCE', payload: eventID});
-    
-    for (let i=0; i<event.length; i++){
-      if(event[i].id == eventID){
-        setEventTitle(event[i].event_title);
-        console.log(event[i].event_title);
-      }
-    }
-  }
-
   return (
     <div>
-    
-    <div>
-      <AttendanceChart eventID={eventID} eventTitle={eventTitle} redraw={true}/>
+      <div style={{"position": "relative", "height": "40vh","width":"80vh"}}>
+        <Bar
+          data={state}
+          options={{
+            plugins: {
+              title:{
+                display:true,
+                text:`Attendance by Cohort at ${props.eventTitle} Event`,
+                fontSize:20,
+                position: 'top'
+              },
+              legend:{
+                display:false,
+                position:'right'
+              }
+            }
+          }}
+        />
+      </div>
+      {JSON.stringify(props)}
     </div>
-
-    <select className="eventAttendanceDropdown" onChange={( event )=>setEventID(event.target.value)}>
-      {event.map(event => 
-        (<option key={event.id} value={event.id} className="eventOptions" >{event.event_title}</option>))}
-    </select>
-    <button id="submitChartBtn" onClick={displayChart}>Display Chart</button>
-    </div>
-    );
+  );
 }
 
-export default DataPage;
+export default AttendanceChart;
