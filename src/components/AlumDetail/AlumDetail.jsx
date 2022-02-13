4b08bd25b2fe9772ddc3bb3a09bb706720abd86c
Francisco Lopez <frankie.b.lopez@gmail.com>
2022-02-13 08:53:37 -0600

    fixed tags/skills/notes modals and search skills is working

--- src/components/AlumDetail/AlumDetail.jsx
+++ src/components/AlumDetail/AlumDetail.jsx
@@ -1,47 +1,4 @@
 
-// import React, { useState,useEffect } from 'react';
-// import Typography from '@mui/material/Typography'
-// import { Link } from 'react-router-dom';
-// import {useSelector,useDispatch} from 'react-redux'
-// import AlumSkills from '../AlumSkills/AlumSkills'
-// import AlumNotes from '../AlumNotes/AlumNotes'
-
-// // Basic functional component structure for React with default state
-// // value setup. When making a new component be sure to replace the
-// // component name AlumDetail with the name for the new component.
-// function AlumDetail(props) {
-//   // Using hooks we're creating local state for a "heading" variable with
-//   // a default value of 'Functional Component'
-//   const dispatch = useDispatch()
-  
-//   const oneAlum = useSelector((store) => store.oneAlum);
-//   const oneAlumNotes = useSelector((store) => store.alumNote);
-//   console.log(oneAlum)
-//   console.log(oneAlumNotes)
-
-//   useEffect(() => {
-//     dispatch({type : "FETCH_ALUMNOTE", payload : oneAlum.id})
-//   }, [dispatch])
-
-
-//   return (
-//     <div> 
-//       <h1>{oneAlum.name}</h1>
-//       <h2>FSE</h2>
-//       <p>{oneAlum.alum_placed ? "Placed" : "Seeking"}</p>
-//       <input type="checkbox" placeholder="placed?" name="placed" id=""/>
-//       <div>
-//         < AlumSkills />
-//       </div>
-//       <div>      
-//         < AlumNotes />
-//       </div>
-//       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat suscipit doloremque consequatur neque eius deserunt accusamus reprehenderit, autem explicabo ex atque ea harum quidem voluptatibus tempora quam dolor quasi sapiente!</p>
-
-//     </div>
-//   );
-// }
-
 import React, { useState,useEffect } from 'react';
 import {Typography,Checkbox,TextField} from '@mui/material'
 import { Link } from 'react-router-dom';
@@ -57,7 +14,6 @@ function AlumDetail(props) {
   // a default value of 'Functional Component'
   const dispatch = useDispatch()
   const oneAlum = useSelector((store) => store.oneAlum);
-
 
   useEffect(() => {
     dispatch({type : "FETCH_ALUMNOTE", payload : oneAlum.id})
@@ -78,7 +34,7 @@ function AlumDetail(props) {
 
   return (
     <div> 
-      <h1>{oneAlum.name}</h1>
+      <h1>{oneAlum.name || oneAlum.alum_name}</h1>
       <h2>FSE</h2>
       <p>{oneAlum.alum_placed ? "Placed" : "Seeking"}</p>
       <p>Events Attended = {oneAlum.event_count}</p>
