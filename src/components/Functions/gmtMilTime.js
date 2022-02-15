bbbb4427a08b079b74725d7220e9b7cbe7f59ad1
CJ Barnes <86207741+cee-jay-bee@users.noreply.github.com>
2022-02-15 12:54:48 -0600

    event note time and date rendering correctly

---
+++ src/components/Functions/gmtMilTime.js
@@ -0,0 +1,27 @@
+const milTime = (time) => { // your input
+
+    time = time.split(':'); // convert to array
+
+    // fetch
+    let hours = Number(time[0]) - 6;
+    let minutes = Number(time[1]);
+
+    // calculate
+    let timeValue;
+
+    if (hours > 0 && hours <= 12) {
+      timeValue= "" + hours;
+    } else if (hours > 12) {
+      timeValue= "" + (hours - 12);
+    } else if (hours == 0) {
+      timeValue= "12";
+    }
+  
+    timeValue += (minutes < 10) ? ":0" + minutes : ":" + minutes;  // get minutes
+    timeValue += (hours >= 12) ? " P.M." : " A.M.";  // get AM/PM
+
+    // show
+    return timeValue;
+  }
+
+export default milTime;
