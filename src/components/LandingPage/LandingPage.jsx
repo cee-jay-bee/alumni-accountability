96a8e72b58695a58f8b5cfd36fe5b975ef2bb9cf
Paran Kashani <parankashani@gmail.com>
2022-02-15 12:48:58 -0600

    landing page in flight

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -19,10 +19,10 @@ function LandingPage() {
             <img id="landingPagePic"src="../images/PriumniLogo.png" alt="" />
             <div className="landingPageLoginFormContainer">
               <LoginForm />
-              <div className="landingMemberEmail">
+              <center className="landingMemberEmail">
                 <h4>Not a Member?</h4>
                 <h4>Email <a href="mailto:christy@primeacademy.io" style={{color: 'white'}}>Christy</a> or <a href="mailto:bellamy@primeacademy.io" style={{color: 'white'}}>Bellamy</a> for Access</h4>
-              </div>
+              </center>
           </div>
       </div>
     </div>
