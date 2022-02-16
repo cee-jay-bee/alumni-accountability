018bbdb83b30bc6a762deb86f7f484edd0c93e6b
Paran Kashani <parankashani@gmail.com>
2022-02-16 16:32:55 -0600

    init styling

--- src/components/AlumDetail/AlumDetail.jsx
+++ src/components/AlumDetail/AlumDetail.jsx
@@ -5,6 +5,8 @@ import { Link } from 'react-router-dom';
 import {useSelector,useDispatch} from 'react-redux'
 import AlumSkills from '../AlumSkills/AlumSkills'
 import AlumNotes from '../AlumNotes/AlumNotes'
+//IMPORT SCSS 
+import '../AlumDetail/AlumDetail.scss'
 
 // Basic functional component structure for React with default state
 // value setup. When making a new component be sure to replace the
@@ -33,39 +35,38 @@ function AlumDetail(props) {
   }
 
   return (
-    <main className="eventDetailMainDiv">
-       <div className='eventDetailHeader'>
-         <div className='eventDetailCol1'>
-          <div className="eventDetailTitleDate">   
-            <h2 id="eventDetailTitle">{oneAlum.name || oneAlum.alum_name}</h2>
-            <h2>FSE</h2>
-            <p>{oneAlum.alum_placed ? "Placed" : "Seeking"}</p>
-            <p>Events Attended = {oneAlum.event_count}</p>
-            <Typography variant="subtitle">Placed</Typography>
-            <Checkbox  checked={oneAlum.alum_placed} onChange={()=>handleCheckbox(oneAlum.id,oneAlum.alum_placed)} />
-            {oneAlum.alum_placed && 
-            <div>
-
-            <input type="date" className="createnewDateInput" autoComplete= "off" 
-            required  onChange={(e)=>placedDateHandler(e,oneAlum.id)} />  
-
+    <main className="alumDetailMainDiv">
+         <div className='alumDetailCol1'>
+            <div className="alumDetailTitleDate">   
+              <h2 id="eventDetailTitle">{oneAlum.name || oneAlum.alum_name}</h2>
             </div>
-            }
-            {oneAlum.alum_placed && oneAlum.placed_date &&
-            <div>
-              <Typography variant="subtitle">Placed on : {oneAlum.placed_date.split("T")[0]}</Typography>
+            <div className="eventDetailStackType">
+              <p>FSE</p>
             </div>
-            }
+            <div className="tobePlacedOrNotToBePlaced">
+                <p>{oneAlum.alum_placed ? "Placed" : "Seeking"}</p>
+                <p>Events Attended = {oneAlum.event_count}</p>
+                <Typography variant="subtitle">Placed</Typography>
+                <Checkbox  checked={oneAlum.alum_placed} onChange={()=>handleCheckbox(oneAlum.id,oneAlum.alum_placed)} />
+                {oneAlum.alum_placed && 
+                <div>
+                    <input type="date" className="createnewDateInput" autoComplete= "off" 
+                    required  onChange={(e)=>placedDateHandler(e,oneAlum.id)} />  
+                </div>
+                }
+                {oneAlum.alum_placed && oneAlum.placed_date &&
+                <div>
+                  <Typography variant="subtitle">Placed on : {oneAlum.placed_date.split("T")[0]}</Typography>
+                </div>
+                }
+            </div>
+        </div>
       <div style={{marginTop:"1rem"}}>
         < AlumSkills />
       </div>
       <div>      
         < AlumNotes />
       </div>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat suscipit doloremque consequatur neque eius deserunt accusamus reprehenderit, autem explicabo ex atque ea harum quidem voluptatibus tempora quam dolor quasi sapiente!</p>
-        </div>
-      </div>
-    </div>
   </main>
   );
 }
