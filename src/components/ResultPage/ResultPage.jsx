650a69d06cf33f91707a1f849121d401c056cfb2
Paran Kashani <parankashani@gmail.com>
2022-02-19 17:58:53 -0600

    results page adjusted

--- src/components/ResultPage/ResultPage.jsx
+++ src/components/ResultPage/ResultPage.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import {useSelector} from 'react-redux';
 import { Select } from 'react-select';
 import AttendanceItem from '../AttendanceItem/AttendanceItem'
+//IMPORT SCSS
+import './ResultPage.scss'
 
 // Basic functional component structure for React with default state
 // value setup. When making a new component be sure to replace the
@@ -14,16 +16,18 @@ function ResultPage(props) {
 
 
   return (
-    <div>
+    <div className="mainAttendanceDiv">
       {/* {JSON.stringify(searchbySkill)} */}
-      <h2>Search ResultPage is: "" </h2>
+      <div className="resultPageHeader">
+        <h2 style={{'font-weight':'500', 'margin-left':'240px'}}>Search Results:</h2>
+      </div>
       {/* append as cards? Perhaps with tag info + cohort info? */}
       <div id='attendanceTableMain'>
           <div className='attendanceTableRow'>
             <div className='attendanceTableCol1'></div>
-            <h3 className='attendanceTableCol2'>Name</h3>
-            <h3 className='attendanceTableCol3'>Cohort</h3>
-            <h3 className='attendanceTableCol4'>Graduation Date</h3>
+            <h3 id='attendanceTableCol2'>Name</h3>
+            <h3 id='attendanceTableCol3'>Cohort</h3>
+            <h3 id='attendanceTableCol4'>Graduation Date</h3>
           </div>
           {searchbySkill.map(alum => 
               (<AttendanceItem key={alum.id} alum={alum} />) 
