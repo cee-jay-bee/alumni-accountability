b69660d7a34441a542a122a5348af4149ca32c08
CJ Barnes <86207741+cee-jay-bee@users.noreply.github.com>
2022-02-19 19:52:11 -0600

    modal pops for deleting user

--- server/routes/eventAttendance.router.js
+++ server/routes/eventAttendance.router.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
+const {
+  rejectUnauthenticated,
+} = require('../modules/authentication-middleware');
 
 /**
  * GET route template
