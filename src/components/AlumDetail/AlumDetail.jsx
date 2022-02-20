9964d1eec4b5cccfa7e4adfc6107f71e4fd0c997
CJ Barnes <86207741+cee-jay-bee@users.noreply.github.com>
2022-02-19 20:59:31 -0600

    alum detail coding fixed

--- src/components/AlumDetail/AlumDetail.jsx
+++ src/components/AlumDetail/AlumDetail.jsx
@@ -36,78 +36,79 @@ function AlumDetail(props) {
 
   return (
     <main className="alumDetailMainDiv">
-         <div className='alumDetailCol1'>
-           <div className="alumDetailTitleandStack">
-                <div className="alumDetailTitleDiv">
-                    <div>   
-                      <h2 id="alumDetailTitle">{oneAlum.name || oneAlum.alum_name}</h2>
-                    </div>
-                    <div className="eventDetailStackType">
-
-                    {(oneAlum.cohort_type === 'FSE') ?
-                            <p className="mainPageStackTypeDisplay" style={{'backgroundColor': '#919f73'}}>FSE</p> :
-                            (oneAlum.cohort_type === 'UXD') ?
-                            <p className="mainPageStackTypeDisplay" style={{'backgroundColor': '#da9595'}}>UXD</p> :
-                            <span><p className="mainPageStackTypeDualDisplay" style={{'backgroundColor': '#da9595'}}>UXD</p> <p class="mainPageStackTypeDualDisplay" style={{'background-color': '#919f73'}}>FSE</p></span>
-                        }
-
-//                       <p>{oneAlum.cohort_type}</p>
-
-                    </div>
-                </div>
-                <div id="placedbookmarkandDate">
-                    {/* <p id="alumDetailseekingMargin">{oneAlum.alum_placed ? "Placed": "Seeking"}</p> */}
-                    {(oneAlum.alum_placed )?
-                        <p id="alumDetailseekingMargin" 
-                            style={{'border-bottom': '30px solid #434169', 'border-top':'30px solid #434169', 'border-left':'30px solid transparent'
-                            }}>Placed</p>:
-                        <p  id="alumDetailseekingMargin" style={{'border-bottom': '30px solid #da9595', 'border-top':'30px solid #da9595', 'border-left':'30px solid transparent'}}>Seeking</p>
-                      }
-                </div>
-           </div>
-           <div className="graddateAndPlaced">
-                <p>Graduation date: {dateChange(oneAlum.graduation_date)}</p> <br/>
-                <p id="eventsAttendedId">Events Attended = {oneAlum.event_count}</p>
-                {/* <p id="alumDetailseekingMargin">{oneAlum.alum_placed ? "Placed" : "Seeking"}</p> */}
+      <div className='alumDetailCol1'>
+        <div className="alumDetailTitleandStack">
+          <div className="alumDetailTitleDiv">
+            <div>   
+              <h2 id="alumDetailTitle">{oneAlum.name || oneAlum.alum_name}</h2>
             </div>
-            <div className="tobePlacedOrNotToBePlaced">
-                <h2 className="placedTitle">Placed<Checkbox  checked={oneAlum.alum_placed} onChange={()=>handleCheckbox(oneAlum.id,oneAlum.alum_placed)} /></h2>
-                {oneAlum.alum_placed && 
+            <div className="eventDetailStackType">
+
+              {(oneAlum.cohort_type === 'FSE') ?
+              <p className="mainPageStackTypeDisplay" style={{'backgroundColor': '#919f73'}}>FSE</p> :
+              (oneAlum.cohort_type === 'UXD') ?
+              <p className="mainPageStackTypeDisplay" style={{'backgroundColor': '#da9595'}}>UXD</p> :
+              <span><p className="mainPageStackTypeDualDisplay" style={{'backgroundColor': '#da9595'}}>UXD</p> <p class="mainPageStackTypeDualDisplay" style={{'background-color': '#919f73'}}>FSE</p></span>
+              }
+
+              {/* <p>{oneAlum.cohort_type}</p> */}
+
+            </div>
+          </div>
+          <div id="placedbookmarkandDate">
+            {/* <p id="alumDetailseekingMargin">{oneAlum.alum_placed ? "Placed": "Seeking"}</p> */}
+            {(oneAlum.alum_placed )?
+              <p id="alumDetailseekingMargin" 
+              style={{'border-bottom': '30px solid #434169', 'border-top':'30px solid #434169', 'border-left':'30px solid transparent'
+            }}>Placed</p>:
+            <p  id="alumDetailseekingMargin" style={{'border-bottom': '30px solid #da9595', 'border-top':'30px solid #da9595', 'border-left':'30px solid transparent'}}>Seeking</p>
+            }
+          </div>
+        </div>
+        <div className="graddateAndPlaced">
+          <p>Graduation date: {dateChange(oneAlum.graduation_date)}</p> <br/>
+          <p id="eventsAttendedId">Events Attended = {oneAlum.event_count}</p>
+          {/* <p id="alumDetailseekingMargin">{oneAlum.alum_placed ? "Placed" : "Seeking"}</p> */}
+        </div>
+        <div className="tobePlacedOrNotToBePlaced">
+          <h2 className="placedTitle">Placed<Checkbox  checked={oneAlum.alum_placed} onChange={()=>handleCheckbox(oneAlum.id,oneAlum.alum_placed)} /></h2>
+          {oneAlum.alum_placed && 
+          // <div>
+          //   <input type="date" className="createnewDateInput" autoComplete= "off" required  onChange={(e)=>placedDateHandler(e,oneAlum.id)} />
+          //     {/* {oneAlum.alum_placed && oneAlum.placed_date &&
+          //       <div className="placedOnDiv">
+          //         <h3 className="placedOnHeader">Placed on: {dateChange(oneAlum.placed_date)}</h3>
+          //       </div>
+          //     }   */}
+          // </div>
+                
+
 
                 <div>
-                    <input type="date" className="createnewDateInput" autoComplete= "off" 
-                    required  onChange={(e)=>placedDateHandler(e,oneAlum.id)} />
-                {oneAlum.alum_placed && oneAlum.placed_date &&
-                <div className="placedOnDiv">
-                  <h3 className="placedOnHeader">Placed on: {dateChange(oneAlum.placed_date)}</h3>
+
+                  <input type="date" className="createnewDateInput" autoComplete= "off" 
+                  
+                  required value={oneAlum.placed_date} onChange={(e)=>placedDateHandler(e,oneAlum.id)} />  
+
                 </div>
-                }  
-                </div>
-                }
-
-
-//             <div>
-
-//             <input type="date" className="createnewDateInput" autoComplete= "off" 
-//             required value={oneAlum.placed_date} onChange={(e)=>placedDateHandler(e,oneAlum.id)} />  
-
-//             </div>
-            }
+}
+          
             {/* {oneAlum.alum_placed && oneAlum.placed_date &&
             <div>
               <Typography variant="subtitle">Placed on : {oneAlum.placed_date.split("T")[0]}</Typography>
             </div>
-            } */}
+            } } */}
+        
 
 
-            </div>
-        </div>
-      <div style={{marginTop:"1rem"}}>
-        < AlumSkills />
       </div>
-      <div>      
-        < AlumNotes />
-      </div>
+    </div>
+    <div style={{marginTop:"1rem"}}>
+      < AlumSkills />
+    </div>
+    <div>      
+      < AlumNotes />
+    </div>
   </main>
   );
 }
